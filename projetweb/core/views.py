6ec6901869315c07e4f877273b0acb281eade18e
Spectral-V <murrisvictor@gmail.com>
2023-12-13 18:16:10 +0100

    On peut créer des comptes !!!

--- projetweb/core/views.py
+++ projetweb/core/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 # Create your views here.
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('accreated')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+    else:
+        return render(request, 'core/signup.html')
+    
+def accreated(request):
+    return render(request, 'core/accreated.html')
