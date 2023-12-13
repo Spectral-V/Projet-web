f405a925cb9fa2024d8e713a40949e3808139bc9
Spectral-V <murrisvictor@gmail.com>
2023-12-13 08:37:12 +0100

    Creation du projet

---
+++ projetweb/core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_imgs', default='blank-profile.png')
+    
+    def __str__(self):
+        return self.user.username
+    
