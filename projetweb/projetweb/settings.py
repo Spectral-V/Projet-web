4a1548a823c7abcd387c528cc0b8584ee9301bff
Spectral-V <murrisvictor@gmail.com>
2024-01-09 10:38:38 +0100

    decoration css

--- projetweb/projetweb/settings.py
+++ projetweb/projetweb/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "projetweb.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
